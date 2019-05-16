c8dcd800251a11216bee0407d3a784c0441bbb5e
James LaPenn <jlapenn@loomissayles.com>
2019-05-16 10:35:33 -0400

    initial commit

---
+++ ItemsControlPopup/HasNamesConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace ItemsControlPopup
+{
+    public class HasNamesConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(value is int count)) return false;
+            return count > 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ ItemsControlPopup/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Microsoft.Expression.Interactivity.Core;
+
+namespace ItemsControlPopup
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        Random rand = new Random();
+        private ObservableCollection<string> names;
+
+        public ObservableCollection<String> Names
+        {
+            get => names;
+            set
+            {
+                names = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddItem
+        {
+            get => new ActionCommand(() =>
+            {
+                var text = String.Concat(Enumerable.Range(0, 10).Select(i => (char)rand.Next()));
+                Names.Add($"Test {text}");
+            });
+        }
+
+
+        public MainViewModel()
+        {
+            Names = new ObservableCollection<string>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged([CallerMemberName]string propertyName = null) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
